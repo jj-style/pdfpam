5191a29256e51d6b3b4ae18f81d155b37bd2ce2e
JJ Style <style.jj@pm.me>
2021-12-21 17:34:55 +0000

    add readme, format code and rename usage

---
+++ README.md
@@ -0,0 +1,28 @@
+# pdfpam
+CLI utility for picking and choosing pages from certain PDF files and combining them together.
+
+## Requirements
+- `pdftk`
+
+## Usage
+`python3 main.py config.txt /path/to/dir/of/pdfs output.pdf`
+
+## Configuration
+The input file must have the following format:  
+```
+1:1 3 5
+2:10 20
+3:5-7 10
+```  
+This would extract pages:
+- 1, 3, 5 from the file starting `1-*` in the directory given
+- 10 and 20 from the file starting `2-*` in the directory given
+- 5,6,7 and 10 from the file starting `3-*` in the directory given
+and combine them all into one file.
+
+**Note the files must all be in the same directory and be identifiable by something followed by a hyphen, e.g. `[1,2,3]-file.pdf`, `[a,b,c]-file.pdf`.**
+
+
+# Roadmap
+- [] Create python package on pypi
+- [] Create a proper CLI with more config options like different glob options for files in the directory, specifying multiple directories
--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import subprocess, shlex, sys
 from tempfile import NamedTemporaryFile as NTF
 from pathlib import Path
 
+
 def get_file_range_dispatch(files, directory):
     dir_p = Path(directory)
     if not dir_p.exists():
         raise Exception(f"directory: {directory} does not exist")
-    
+
     with open(files, "r") as f:
         frange = f.readlines()
 
     frd = {}
 
     for f in frange:
-        fno,page_range = (f.split(":"))
+        fno, page_range = f.split(":")
         page_range = page_range.strip()
         globbed = list(dir_p.glob(f"{fno}-*.pdf"))
         if len(globbed) != 1:
@@ -22,36 +23,45 @@ def get_file_range_dispatch(files, directory):
         frd[thefile] = page_range
     return frd
 
+
 def main():
     args = sys.argv[1:]
     if len(args) < 3:
-        print("usage: collator file.txt directory output", file=sys.stderr)
+        print("usage: pdfpam input directory output", file=sys.stderr)
         exit(1)
-    file,dirr,output = args[0], args[1], args[2]
+    file, dirr, output = args[0], args[1], args[2]
 
     try:
         frd = get_file_range_dispatch(file, dirr)
     except Exception as e:
         print(e, file=sys.stderr)
         exit(1)
-    
+
     out = Path(output)
     if out.exists():
-        print(f"warning: {out} already exists. Aborting to prevent overwriting data", file=sys.stderr)
+        print(
+            f"warning: {out} already exists. Aborting to prevent overwriting data",
+            file=sys.stderr,
+        )
         exit(1)
 
     tmp1 = NTF(suffix=".pdf")
     tmp2 = NTF(suffix=".pdf")
 
-    for file,page_range in frd.items():
-        if not out.exists(): # first run
+    for file, page_range in frd.items():
+        if not out.exists():  # first run
             subprocess.run(shlex.split(f"pdftk {file} cat {page_range} output {out}"))
         else:
-            subprocess.run(shlex.split(f"pdftk {file} cat {page_range} output {tmp1.name}"))
+            subprocess.run(
+                shlex.split(f"pdftk {file} cat {page_range} output {tmp1.name}")
+            )
             subprocess.run(shlex.split(f"cp {out} {tmp2.name}"))
-            subprocess.run(shlex.split(f"pdftk {tmp2.name} {tmp1.name} cat output {out}"))
+            subprocess.run(
+                shlex.split(f"pdftk {tmp2.name} {tmp1.name} cat output {out}")
+            )
     tmp1.close()
     tmp2.close()
+
 
 if __name__ == "__main__":
     main()
